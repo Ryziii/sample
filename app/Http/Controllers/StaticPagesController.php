bb41112d540eedba2cd5d8cd31bdebf42f7498cd
Ryziii <az.convict@hotmail.com>
2018-08-12 15:19:47 +0000

    Finish layout and toutes

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,10 +8,10 @@ class StaticPagesController extends Controller
 {
     //
     public function home(){
-        return view('static_pages/home');
+        return view('static_pages.home');
     }
     public function help(){
-        return view('static_pages/help');
+        return view('static_pages.help');
     }
     public function about(){
         return view('static_pages/about');
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
-@section('title','主页')
 @section('content')
-    <h1>主页test</h1>
+    <div class="jumbotron">
+        <h1 >Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="http://ryziiisample.hersoku.com">我的主页</a>
+        </p>
+        <p>
+            一切从这里开始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a>
+        </p>
+    </div>
 @stop
